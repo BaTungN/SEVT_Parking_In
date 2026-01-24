3afb4f6b4cdbce1a44d6c26de31970fb3f860255
BaTungN <103803799+BaTungN@users.noreply.github.com>
2026-01-24 14:31:24 +0700

    Update Checkin_MODE.py

--- Checkin_MODE.py
+++ Checkin_MODE.py
@@ -4,6 +4,7 @@ import logging
 from datetime import datetime, timedelta,timezone
 from datetime import timezone, timedelta, timedelta as td
 import serial
+import schedule
 import time
 from ExtensionCls.MongoDB import MongoDB
 from ExtensionCls.IsCheckTime import IsCheckTime
@@ -400,9 +401,24 @@ def main():
                                 current_tag=tag
                                 #on_pin()
                                 logging.info("========================== ID nhan duoc: {} =====================================".format(tag))
-                                checkin = ControlCar(nameparking=_name_parking, checktime_set=45)
-                                logging.info(">>>>===Step 1")
-                                checkin.checkin_car(tag)
+                                try:
+                                    checkin = ControlCar(nameparking=_name_parking, checktime_set=45)
+                                    logging.info(">>>>===Step 1")
+                                    checkin.checkin_car(tag)
+                                except:
+                                    check=False
+                                    logging.info(">>>>===Step 1 OFFLINE")
+                                    with open("cache_collection.json", "r", encoding="utf-8") as f:
+                                        offline_data = json.load(f, object_hook=json_util.object_hook)
+                                    for off in offline_data:
+                                        if off["id_card"]["sha"]== hash_sha256(tag[-8:]) and off["name_parking"] ==_name_parking and off["type_card"]["sha"] == hash_sha256("epass"):
+                                            check=True
+                                            on_pin()
+                                    if check==False:
+                                        for off in offline_data:
+                                            if off["id_card"]["sha"] == hash_sha256(tag) and off["name_parking"] ==_name_parking and off["type_card"]["sha"] == hash_sha256("vetc"):
+                                                check=True
+                                                on_pin()
                                 state ="CARD_HELD"
                                 last_seen = now
                         elif state =="CARD_HELD":
@@ -418,8 +434,23 @@ def main():
                                 #on_pin()
                                 logging.info(">>>>===Step 3")
                                 logging.info("========================== ID nhan duoc: {} ========================== ".format(tag))
-                                checkin = ControlCar(nameparking=_name_parking, checktime_set=45)
-                                checkin.checkin_car(tag)
+                                try:
+                                    checkin = ControlCar(nameparking=_name_parking, checktime_set=45)
+                                    checkin.checkin_car(tag)
+                                except:
+                                    check=False
+                                    logging.info(">>>>===Step 3 OFFLINE")
+                                    with open("cache_collection.json", "r", encoding="utf-8") as f:
+                                        offline_data = json.load(f, object_hook=json_util.object_hook)
+                                    for off in offline_data:
+                                        if off["id_card"]["sha"]== hash_sha256(tag[-8:]) and off["name_parking"] ==_name_parking and off["type_card"]["sha"] == hash_sha256("epass"):
+                                            check=True
+                                            on_pin()
+                                    if check==False:
+                                        for off in offline_data:
+                                            if off["id_card"]["sha"] == hash_sha256(tag) and off["name_parking"] ==_name_parking and off["type_card"]["sha"] == hash_sha256("vetc"):
+                                                check=True
+                                                on_pin()
                                 
                                 current_tag=tag
                                 last_seen=now
@@ -433,13 +464,48 @@ def main():
         print("FAIL COM")
                 
 #====================================================================================
+#====================================================================================
+#====================================================================================
+#==========================      AUTO BACKUP      ===================================
+#====================================================================================
+def backup_collection():
+    MONGO_URI = configs['server']['uri']
+    DB_NAME = configs['server']['db_name']
+    BACKUP_FILE = "./backup.json"   
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client[DB_NAME]
+        vehicles = db["EmployeeParking"]
+        data = list(vehicles.find({}))
 
+        tmp_file = "cache_collection.json.tmp"
+
+        with open(tmp_file, "w", encoding="utf-8") as f:
+            json.dump(data, f, default=json_util.default, ensure_ascii=False)
+
+        os.replace(tmp_file, "cache_collection.json")
+
+        print(f"? Backup OK [{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]")
+
+    except Exception as e:
+        print(f"? Backup loii: {e}")
+schedule.every().day.at("00:00").do(backup_collection)
+schedule.every().day.at("17:00").do(backup_collection)
+# ===== LOOP CH?Y N?N =====
+def backup():
+    logging.info("Backup Start")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+#====================================================================================
 def _main():
     
     thread = threading.Thread(target=thread_checkin, args=(port_name, baudrate))
     # thread = threading.Thread(target=thread_checkin, args=("COM1", 9600))
     thread.start()
-
+    thread2 = threading.Thread(target=backup)
+    # thread = threading.Thread(target=thread_checkin, args=("COM1", 9600))
+    thread2.start()
 if __name__ == '__main__':
     # main_oneway("/dev/ttyUSB0", 57600)
   
